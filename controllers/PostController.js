802423f92cd06e3179cb3c4c139936517005b7da
SenkeSem <sen9morf@gmail.com>
2023-08-30 18:02:17 +0300

    v1.1

---
+++ controllers/PostController.js
@@ -0,0 +1,22 @@
+import PostModel from "../models/Post.js";
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      tags: req.body.tags,
+      imageUrl: req.body.imageUrl,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      message: 'Не удалось создать статью',
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,42 +1,29 @@
 import express from 'express';
-import jwt from 'jsonwebtoken'; 
-import bcrypt from 'bcrypt';
-import mongoose from 'mongoose';
+import mongoose, { get } from 'mongoose';
+import { reqisterValidation, loginValidation, postCreateValidation } from './validations.js';
+import checkAuth from './utils/checkAuth.js';
 
-import { validationResult } from 'express-validator';
-import { reqisterValidation } from './validations/auth.js';
-import UserModel from './models/User.js';
+import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 mongoose
-.connect('mongodb+srv://sensem2012:xF7HJ3N5RpP5J1ce@cluster0.xz023gx.mongodb.net/?retryWrites=true&w=majority')
-.then(() => console.log('DB okey'))
-.catch((err) => console.log('DB error', err));
+  .connect('mongodb+srv://sensem2012:xF7HJ3N5RpP5J1ce@cluster0.xz023gx.mongodb.net/blog?retryWrites=true&w=majority')
+  .then(() => console.log('DB okey'))
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/auth/reqister', reqisterValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+app.post('/auth/login', loginValidation,UserController.login);
+app.post('/auth/reqister', reqisterValidation, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-
-  res.json(user);
-});
+// app.get('/posts', PostController.getAll);
+// app.get('/posts/:id', PostController.getOne);
+app.post('/posts', postCreateValidation, PostController.create);
+// app.delete('/posts', PostController.remove);
+// app.patch('/posts', PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
